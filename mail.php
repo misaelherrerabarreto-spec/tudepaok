10f1d42674b926bcac6c112859d276f11eca919d
Misael Herrera HOME <misaelhb.21@gmail.com>
2025-11-10 17:17:46 -0500

    REAL BASE VERSION

---
+++ mail.php
@@ -0,0 +1,30 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+
+$EmailTo = "samu.arone@gmail.com";
+$Title = "New Message Received - sam";
+
+// prepare email body text
+$Fields .= "Name: ";
+$Fields .= $name;
+$Fields .= "\n";
+
+$Fields.= "Email: ";
+$Fields .= $email;
+$Fields .= "\n";
+
+$Fields.= "Subject: ";
+$Fields .= $subject;
+$Fields .= "\n";
+
+$Fields .= "Message: ";
+$Fields .= $message;
+$Fields .= "\n";
+
+
+// send email
+$success = mail($EmailTo,  $Title,  $Fields, "From:".$email);
